bbb6b17688ce47bb50aef21506f94b5d041ada5b
K-Break <kevin.morillo135@gmail.com>
2026-05-29 15:19:40 +0200

    Fix : changement target server proxy vers http://api:3000

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000',
         changeOrigin: true,
       },
     },
   },
-})
+})
