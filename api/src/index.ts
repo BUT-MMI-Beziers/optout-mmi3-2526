7d03e412951daa551d3dd45f357de3aab948cb3f
Fabien Bonnemayre <fabien@MacBook-Air-de-Fabien.local>
2026-06-08 10:54:16 +0200

    feat: add notifications table, GET /users/me/notifications and PATCH /users/me/notifications/:id endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,12 +3,16 @@ import { Hono } from 'hono'
 import brokersRoute from './routes/brokers.routes.js'
 import templatesRoutes from './routes/templates.routes.js'
 import requestsRoutes from './routes/requests.routes.js'
+import { usersRoutes } from './routes/users.routes.js'
+
 
 const app = new Hono()
 
 app.route('/api/v1/brokers', brokersRoute)
 app.route('/api/v1/templates', templatesRoutes)
 app.route('/api/v1/requests', requestsRoutes)
+app.route('/api/v1/users', usersRoutes)
+
 
 
 const port = Number(process.env.API_PORT_INTERNAL) || 3000
